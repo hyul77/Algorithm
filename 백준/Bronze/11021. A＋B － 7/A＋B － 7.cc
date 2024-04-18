76f59bdbc65d8510f6ffad8072cd9912b7ccf1fc
이준혁 <100561170+hyul77@users.noreply.github.com>
2024-04-18 23:46:47 +0900

    [Bronze V] Title: A+B - 7, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int T,X,Y;
+    cin>>T;
+    for(int i =0; i<T; i++){
+        cin>>X>>Y;
+        cout<<"Case #"<<i+1<<": "<<X+Y<<"\n";
+    }
+    
+    
+    
+    return 0;
+}
