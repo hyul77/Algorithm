d82605047a0d4de14a3e3f3f5c91e9001c3a397d
이준혁 <100561170+hyul77@users.noreply.github.com>
2024-10-05 11:23:19 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -0,0 +1,7 @@
+-- 코드를 입력하세요
+SELECT AI.NAME, AI.DATETIME
+from ANIMAL_INS AI
+left join ANIMAL_OUTS AO on AI.ANIMAL_ID = AO.ANIMAL_ID
+where AO.ANIMAL_ID is null
+order by AI.DATETIME ASC
+limit 3;
