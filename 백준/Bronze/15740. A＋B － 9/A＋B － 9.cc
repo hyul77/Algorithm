0db00ba89797125c68c29f46bee93b606ea705aa
이준혁 <100561170+hyul77@users.noreply.github.com>
2025-01-06 12:30:53 +0900

    [Bronze V] Title: A+B - 9, Time: 0 ms, Memory: 2020 KB, Score: 75 point -BaekjoonHub

---
+++ 백준/Bronze/15740. A＋B － 9/A＋B － 9.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+int main() {
+	
+	long long A, B;
+
+	cin >> A >> B;
+
+	cout << A + B << "\n";
+
+
+
+
+	return 0;
+}
