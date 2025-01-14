206393f25a07b477eb1205f493f45c1358f9dc6f
이준혁 <100561170+hyul77@users.noreply.github.com>
2025-01-15 02:17:19 +0900

    Title: 청소당번 정하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250115/청소당번 정하기/cleaning-numbering.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+    int cnt12 = 0;
+    int cnt3 = 0;
+    int cnt2 = 0;
+
+    for (int i = 1; i <= n; i++) {
+        if(i % 12 == 0) {
+            cnt12++;
+        }
+        else if (i % 3 == 0) {
+            cnt3++;
+        }
+        else if (i % 2 == 0) {
+            cnt2++;
+        }else {
+            continue;
+        }
+    }
+    cout << cnt2 << " " << cnt3 << " " << cnt12;
+
+    return 0;
+}
