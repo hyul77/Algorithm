ead883694e2b7086d823c18e5110e2e70cf319b7
이준혁 <100561170+hyul77@users.noreply.github.com>
2025-01-14 18:07:38 +0900

    Title: 2개 중 최대, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250114/2개 중 최대/max-of-two-nums.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    cin >> a >> b;
+
+    int rst = a > b ? a : b;
+    cout << rst;
+    return 0;
+}
