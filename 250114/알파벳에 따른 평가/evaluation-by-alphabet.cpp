047252cf3544ff04c74d483c3cdb912cd3a498ed
이준혁 <100561170+hyul77@users.noreply.github.com>
2025-01-14 18:34:55 +0900

    Title: 알파벳에 따른 평가, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250114/알파벳에 따른 평가/evaluation-by-alphabet.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    char a;
+    cin >> a;
+
+    switch (a) {
+        case 'S':
+            cout << "Superior";
+            break;
+        case 'A':
+            cout << "Excellent";
+            break;
+        case 'B':
+            cout << "Good";
+            break;
+        case 'C':
+            cout << "Usually";
+            break;
+        default:
+            cout << "Failure";
+    }
+    return 0;
+}
