fd701ae7493683798669e74147169c1ffac61e78
이준혁 <100561170+hyul77@users.noreply.github.com>
2025-01-15 01:45:42 +0900

    Title: 369 게임, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250115/369 게임/369-game.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        if (i % 3 == 0) {
+            cout << "0 ";
+        }
+        else if(i % 10 == 3 || i % 10 == 6 || i % 10 == 9) {
+            cout << "0 ";
+        }
+        else {
+            cout << i << " ";
+        }
+    }
+    return 0;
+}
