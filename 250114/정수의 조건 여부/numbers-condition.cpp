9e6daab5b3614dd456ea41d60d0470f9d5a8a60d
이준혁 <100561170+hyul77@users.noreply.github.com>
2025-01-14 18:01:33 +0900

    Title: 정수의 조건 여부, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250114/정수의 조건 여부/numbers-condition.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin >> a;
+
+    if (a >=13) {
+        cout << "1";
+    }
+    else {
+        cout << "0";
+    }
+    return 0;
+}
