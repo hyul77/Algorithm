fea1ec0e0a5cc097bc69213cbe91d9d72e580244
이준혁 <100561170+hyul77@users.noreply.github.com>
2025-01-14 18:14:07 +0900

    Title: 삼항연산자 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250114/삼항연산자 2/ternary-operator-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin >> a;
+
+    string rst = (a == 1) ? "t" : "f";
+    cout << rst;
+    return 0;
+}
