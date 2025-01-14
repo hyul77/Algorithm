32efb20b7ed73eb5fc09653eb231ae62a59aeda6
이준혁 <100561170+hyul77@users.noreply.github.com>
2025-01-14 23:00:04 +0900

    Title: 세 정수의 최솟값, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250114/세 정수의 최솟값/minimum-of-three-numbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    int min = a;
+
+    if (min > b) min = b;
+    if (min > c) min = c;
+
+    cout << min;
+
+    return 0;
+}
