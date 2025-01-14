302077437dbca8597f017c1d958567de16a5a8be
이준혁 <100561170+hyul77@users.noreply.github.com>
2025-01-14 23:50:24 +0900

    Title: 일 수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250114/일 수 구하기/number-of-days-in-month.cpp
+++ 250114/일 수 구하기/number-of-days-in-month.cpp
@@ -9,11 +9,18 @@ int main() {
     if (n == 2) {
         cout << "28";
     }
-    else if (n % 2 != 0 || n == 8) {
-        cout << "31";
-    }
-    else {
-        cout << "30";
+    else if (n < 8) {
+        if (n % 2 == 0) {
+            cout << "30";
+        }else {
+            cout << "31";
+        }
+    }else {
+        if (n % 2 == 0) {
+            cout << "31";
+        }else {
+            cout << "30";
+        }
     }
     return 0;
 }
