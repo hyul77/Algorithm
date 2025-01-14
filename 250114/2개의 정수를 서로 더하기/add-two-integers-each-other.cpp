fac8de6d72d3d78683e38ce0b4feadf517948f83
이준혁 <100561170+hyul77@users.noreply.github.com>
2025-01-14 17:20:22 +0900

    Title: 2개의 정수를 서로 더하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250114/2개의 정수를 서로 더하기/add-two-integers-each-other.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    cin >> a >> b;
+
+    int c = a+b;
+    cout << c << " " << c+b;
+
+    return 0;
+}
