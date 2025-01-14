5fecc48ca8e1309715b4529fd8e7c9bc0500f0a6
이준혁 <100561170+hyul77@users.noreply.github.com>
2025-01-14 17:04:29 +0900

    Title: 전화번호 바꾸기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250114/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, d, v;
+    cin >> a;
+    cin.get();
+    cin >> d;
+    cin.get();
+    cin >> v;
+
+    cout << "0" << a << "-" << v << "-" << d;
+    return 0;
+}
