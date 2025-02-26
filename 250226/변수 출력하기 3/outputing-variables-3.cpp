397f22b559b5b33ef47c789826a663099e617a24
이준혁 <100561170+hyul77@users.noreply.github.com>
2025-02-26 16:59:07 +0900

    Title: 변수 출력하기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250226/변수 출력하기 3/outputing-variables-3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 1;
+    int b = 2;
+    char c = 'C';
+
+    cout << a << "->" << b << "->" << c;
+
+    return 0;
+}
